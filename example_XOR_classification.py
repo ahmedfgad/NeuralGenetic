5a15281abcac87e4e7e807fb93eb122cd05f56cd
Ahmed Gad <ahmed.f.gad@gmail.com>
2021-06-17 22:39:23 -0400

    Replace plot_result() by plot_fitness()

--- example_XOR_classification.py
+++ example_XOR_classification.py
@@ -98,7 +98,7 @@ ga_instance = pygad.GA(num_generations=num_generations,
 ga_instance.run()
 
 # After the generations complete, some plots are showed that summarize how the outputs/fitness values evolve over generations.
-ga_instance.plot_result()
+ga_instance.plot_fitness()
 
 # Returning the details of the best solution.
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
